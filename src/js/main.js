025676cd664da6d350689015925296fd1488b325
irinkwink <irina-ns@yandex.ru>
2022-10-08 07:27:51 +0300

    feat: add modal with flats

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
   var floorPath = $('.home-image path');
   var counterUp = $('.counter-up');
   var counterDown = $('.counter-down');
+  var modal = $('.modal');
+  var modalClose = $('.modal-close');
+  var viewFlatsBtn = $('.view-flats');
+  var flatPath = $('.flats path');
+  var flatLink = $('.flat-link');
 
   function showFloor(floor) {
     $('.counter').text(floor);
@@ -11,10 +16,30 @@ $(document).ready(function () {
     $(`[data-floor = ${floor}]`).toggleClass('current-floor');
   }
 
+  function showFlat(flat) {
+    flatPath.removeClass('current-flat');
+    flatLink.removeClass('current-flat-link');
+    $(`[data-flat = ${flat}]`).toggleClass('current-flat');
+    $(`[data-flat-num = ${flat}]`).toggleClass('current-flat-link');
+  }
+
   floorPath.on("mouseover", function () {
     currentFloor = $(this).attr('data-floor');
     showFloor(currentFloor);
   })
+
+  flatPath.on("mouseover", function () {
+    showFlat($(this).attr('data-flat'));
+  })
+
+  flatLink.on("mouseover", function () {
+    showFlat($(this).attr('data-flat-num'));
+  })
+
+  floorPath.on("click", toggleModal);
+  viewFlatsBtn.on("click", toggleModal);
+
+  modalClose.on("click", toggleModal);
 
   counterUp.on("click", function () {
     if (currentFloor < 18) {
@@ -30,5 +55,9 @@ $(document).ready(function () {
       usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
       showFloor(usCurrentFloor);
     }
-  })
+  });
+
+  function toggleModal() {
+    modal.toggleClass('is-open');
+  }
 });
